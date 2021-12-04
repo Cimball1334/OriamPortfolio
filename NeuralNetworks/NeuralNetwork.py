cf52b8922f56c3a95f10710a30176023c91eb84a
Craig Kimball <kimballcraig1334@gmail.com>
2021-12-04 01:43:41 -0500

    Pushing to git from VSCode instead

--- NeuralNetworks/NeuralNetwork.py
+++ NeuralNetworks/NeuralNetwork.py
@@ -1,18 +1,49 @@
 import numpy as np
 
+
 class NeuralNetwork:
 
     def __init__(self, layer_sizes):
+        #creates a tuple where a is the number of columns and b is the number of rows
+        #this comes from creating a matrix of our connections between neurons
         weight_shapes = [(a,b) for a,b in zip(layer_sizes[1:],layer_sizes[:-1])]
+        
+        #Stores the actual weight values for our weight shapes
+        #()/s[1]**.5 normalizes all the values by dividing by the sqrt of the number of inputs to that layer s[1] so that values do not scale by size of the array             
+        #sqrt info came from: https://cs231n.github.io/neural-networks-2/#init
         self.weights = [np.random.standard_normal(s)/s[1]**.5 for s in weight_shapes]
+        #Stores the biases for each neuron connection
         self.biases = [np.zeros((s,1)) for s in layer_sizes[1:]]
+        
+        # print(weight_shapes)
+        # print(self.weights)
+        # print(self.biases)
 
     def predict(self, a):
+        #this loop represents each layer where w is a specific layers weight and b is the bias
         for w,b in zip(self.weights,self.biases):
+            # i  = np.matmul(w,a)+b
+            # print(i[0])
+            
+            #matrix multiplication between the weights, and the previous neurons output, plus the bias for each neuron
+            #a = output of this layer: activation function passing in the weight, previous layer, and adding bias as defined above
             a = self.activation(np.matmul(w,a) + b)
+        #this is the output layer
         return a
 
+    #determines how accurate our weights nad biases were in determining the proper output
+    def print_accuracy(self,images,labels):
+        predictions = self.predict(images)
+        #total number correct by checking the sum of every time the max number in our prediction matched the max number of our expected output
+        num_correct = sum([np.argmax(a) == np.argmax(b) for a,b in zip(predictions,labels)])
+        #print statement that dispays part/whole of correct and its percentage
+        print('{}/{} accuracy: {}%'.format(num_correct,len(images),(num_correct/len(images))*100))
+
     @staticmethod
+    #sigmoid activation method is f(x) = 1/1+(e^-x) 
+    #activation method is required for non linear function
+    #if you plot the data of a binary question, there are regions where it is either yes or no, 
+    #this is easy to imagine in 2d space as it can be represented on a graph, the more possible outcomes the more dimensions you have
     def activation(x):
         return 1/(1+np.exp(-x))
     
--- NeuralNetworks/Runner.py
+++ NeuralNetworks/Runner.py
@@ -1,8 +1,28 @@
-import neuralnetwork as nn
+import NeuralNetwork as nn
 import numpy as np
+import matplotlib.pyplot as plot
 
-layer_sizes = (3,5,10)
-x = np.ones((layer_sizes[0],1))
+with np.load('mnist.npz') as data:
+    training_images = data['training_images']
+    training_labels = data['training_labels']
+    
+'''visualization code:'''
+
+# plot.imshow(training_images[0].reshape(28,28), cmap = 'gray')
+# plot.show()
+
+layer_sizes = (784,5,10)
+
+'''simulated input'''
+# x = np.ones((layer_sizes[0],1))
 
 net = nn.NeuralNetwork(layer_sizes)
-prediction = net.predict(x)
+prediction = net.predict(training_images)
+
+'''prints the dimension of the matrix'''
+# print(prediction.shape)
+
+#prints the highest prediciton - in this case also the number that it believes that it is
+#print('Number is a {} with ({:.2f}%) confidence'.format( np.argmax(prediction[0]) , prediction[0][np.argmax(prediction[0])][0]*100))
+
+# net.print_accuracy(training_images,training_labels)
