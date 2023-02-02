6c3952238cc2556bbb7bcfe6f98234c16fbffd8b
Cimball1334 <kimballcraig1334@gmail.com>
2023-02-02 15:37:27 -0500

    small changes, added hard coded inputs to bypass user input

--- GMUCS/SST 230/Example Files/excersize.py
+++ GMUCS/SST 230/Example Files/excersize.py
@@ -5,18 +5,16 @@
 
 #Excersize 1
 
-def trimbyn(arr, n):
-    return sorted(arr)[n:-n]
+# def trimbyn(arr, n):
+#     return sorted(arr)[n:-n]
 
-arr = []
-x = int(input('Enter the number of entries? '))
-for i in range(x):
-    arr.append(int(input()))
+# arr = []
+# x = int(input('Enter the number of entries? '))
+# for i in range(x):
+#     arr.append(int(input()))
 
-y = int(input('How many elements would you like to remove? '))
-if len(arr) < 2*y:
-    print('Array Too Short')
-else:
-    print(trimbyn(arr,y),arr)
-
-
+# y = int(input('How many elements would you like to remove? '))
+# if len(arr) < 2*y:
+#     print('Array Too Short')
+# else:
+#     print(trimbyn(arr,y),arr)
